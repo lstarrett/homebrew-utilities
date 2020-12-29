5f72d64e8785ab72293be0cb710144cd274e6559
Lucas Starrett <lucas.starrett@analog.com>
2020-12-28 22:29:28 -0500

    Fix plist block syntax

--- cpu-sentinel.rb
+++ cpu-sentinel.rb
@@ -16,7 +16,7 @@ class CpuSentinel < Formula
 
 	# https://docs.brew.sh/Formula-Cookbook#launchd-plist-files
 	# https://developer.apple.com/library/archive/documentation/MacOSX/Conceptual/BPSystemStartup/Chapters/CreatingLaunchdJobs.html
-	def plist; <<-EOS.undent
+	def plist; <<~EOS
 		<?xml version="1.0" encoding="UTF-8"?>
 		<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 		<plist version="1.0">
